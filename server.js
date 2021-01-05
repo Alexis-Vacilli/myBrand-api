d4c2467536c5835c9cc8c5469129a7052b23d853
Alexis-Vacilli <alexisvacilli1@gmail.com>
2021-01-05 15:21:54 +0200

    mend

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import {} from 'dotenv/config';
+import routes from './src/routes/index'
 import express from 'express';
 
 const app = express();
 
 app.use(express.json());
-
+app.use('/api/v1', routes)
 app.get('/', (req, res) => res.status(200).send({
     status: 200,
     message: `Welcome to Alexis Vacilli's API!`
