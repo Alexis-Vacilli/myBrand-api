afd56ff12e2e7b96c6c9ed918e7320d265af1c10
Alexis-Vacilli <alexisvacilli1@gmail.com>
2021-01-06 09:24:28 +0200

    added api documentation

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,9 +1,40 @@
+import {config} from 'dotenv'; 
 import express from 'express';
 import register from '../../controllers/register';
-import validator from "../../middleware/validatePassword"
+import validator from "../../middleware/validatePassword";
+
 const router = express.Router();
-require ('dotenv').config();
+config();
 
 router.post('/', validator,register);
 
 export default router;
+
+/**
+ * @swagger
+ * /signup:
+ *   post:
+ *     tags:
+ *       - Users
+ *     name: Signup
+ *     summary: Creates a new user
+ *     consumes:
+ *       - application/json
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                email:
+ *                 type: string
+ *                password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *             description: user created successfully.
+ *       409:
+ *             description: The email is already in the system.
+ *       500:
+ *             description: Bad request.
+ * */
