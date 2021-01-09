e41046c733fa570e91a93fda091afb4945a89ba7
Alexis-Vacilli <alexisvacilli1@gmail.com>
2021-01-09 13:55:29 +0200

    Adding Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,6 @@ import documentationRouter from '../documentaion/index';
 const router = Router();
 router.use("/articles", createRoutes);
 router.use('/docs', documentationRouter);
+router.use("/login", userRoutes);
+router.use('/docs', documentationRouter);
 export default router;
